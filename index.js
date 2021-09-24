655311076e878502bbc9ff7f3fd5b91ae9ea27a2
Festus Alabo Augustine <43374293+Asoft123@users.noreply.github.com>
2021-09-24 13:46:57 +0100

    debugging database connection to db4free

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+const { sequelize } = require("./db/models");
 const Main = require("./app");
 
 function startApp() {
   const { app } = Main;
   let port = process.env.PORT || 4000;
-  app.listen(port, (err, success) => {
+  app.listen(port, async (err, success) => {
     if (err) throw err;
     console.log(`Server start on port: ${port}`);
+    await sequelize.sync({ force: true });
+    console.log("Database synched");
   });
 }
 
